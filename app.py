ea783de78479389fc23c95eae3c5e95a34b07375
113mohitsingh <sbswx020mohits@gmail.com>
2026-02-21 23:28:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 st.title("KNN Weather Classification")
-X=np.array([[30, 70],[25, 80],[27, 60],[31, 65],[23, 85],[28, 75]]) 
+X=np.array([[30, 70],[25, 75],[27, 70],[31, 70],[23, 85],[28, 80]]) 
 y=np.array([0, 1, 0, 0, 1, 1])
 knn=KNeighborsClassifier(n_neighbors=3) 
 knn.fit(X, y) 
-new_point=np.array ([[26, 78]]) 
+new_point=np.array ([[26, 80]]) 
 prediction=knn.predict(new_point)[0] 
 plt.figure(figsize=(7, 5)) 
 plt.scatter(X[y==0, 0], X[y==0, 1], label="Sunny", s=100) 
